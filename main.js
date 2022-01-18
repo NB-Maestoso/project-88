257c7e2240b4a7d3579ca4dff721fceee3742202
NB-Maestoso <95860271+NB-Maestoso@users.noreply.github.com>
2022-01-18 13:20:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,118 @@
+canvas = new.fabric.Canvas("myCanvas")
+// Create canvas variable
+
+//Set initial positions for ball and hole images.
+ball_x = 0;
+ball_y = 0;
+hole_x = 1000;
+hole_y = 400;
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img() {
+    // write code to Upload golf image on the canvas
+    new_image();
+    fabric.Image.fromURL("golf-h", function (Img) {
+        hole_obj = Img;
+        hole_obj.scaleToHeight(hole_y);
+        hole_obj.scaleToWidth(hole_x);
+        hole_obj.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(hole_obj);
+    });
+}
+
+function new_image() {
+    // write code to Upload ball image on canvas
+    fabric.Image.fromURL("ball.png", function (Img) {
+        ball_obj = Img;
+        ball_obj.scaleToHeight(ball_y);
+        ball_obj.scaleToWidth(ball_x);
+        ball_obj.set({
+            top: 0,
+            left: 0
+        });
+    });
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    /* Check the coordinates of the ball and hole images to finish the game. 
+    And id coordinates matches them remove ball image, 
+    display "GAME OVER!!!" 
+    and make canvas border 'red'. */
+    if (ball_x == hole_x) && (ball_y == hole_y) {
+        canvas.remove(ball_obj);
+        document.getElementById("hd3").innerHTML("Congratulations Player! You've hit the Goal! Game Over!!!");
+        document.getElementById("myCanvas").style.borderColor = "red";
+    }
+    else {
+        if (keyPressed == '38') {
+            up();
+            console.log("up");
+        }
+        if (keyPressed == '40') {
+            down();
+            console.log("down");
+        }
+        if (keyPressed == '37') {
+            left();
+            console.log("left");
+        }
+        if (keyPressed == '39') {
+            right();
+            console.log("right");
+        }
+    }
+
+    function up() {
+        // Write a code to move ball upward.
+        if (ball_y >= 0) {
+            ball_y = ball_y - block_image_height;
+            console.log("block image height is " + block_image_height);
+            console.log("Ball's current X position = " + ball_x + "| Ball's current Y position =" + ball_y);
+            canvas.remove(ball_obj);
+            new_image();
+        }
+    }
+
+    function down() {
+        // Write a code to move ball downward.
+        if (ball_y <= 400){
+             ball_y = ball_y + block_image_height;
+            console.log("block image height is " + block_image_height);
+            console.log("Ball's current X position = " + ball_x + "| Ball's current Y position =" + ball_y);
+            canvas.remove(ball_obj);
+            new_image();
+        }
+    }
+
+    function left() {
+        if (ball_x > 5) {
+            // Write a code to move ball left side.
+             ball_x = ball_x - block_image_width;
+            console.log("block image width is " + block_image_width);
+            console.log("Ball's current X position = " + ball_x + "| Ball's current Y position =" + ball_y);
+            canvas.remove(ball_obj);
+            new_image();
+        }
+    }
+
+    function right() {
+        if (ball_x <= 1050) {
+            // Write a code to move ball right side.
+             ball_x = ball_x + block_image_width;
+            console.log("block image width is " + block_image_width);
+            console.log("Ball's current X position = " + ball_x + "| Ball's current Y position =" + ball_y);
+            canvas.remove(ball_obj);
+            new_image();
+        }
+    }
+
+}
